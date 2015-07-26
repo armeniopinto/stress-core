82ce7b1f956d28f8f9904add90ec7d6d6cc5ad10
Armenio Pinto <armeniopinto@gmail.com>
2015-07-26 23:34:46 +0100

    Minor changes to the main program.

--- StressCore.cpp
+++ StressCore.cpp
@@ -8,12 +8,13 @@
  */
 
 #include "Arduino.h"
+
 #include "StressCore.h"
 
 Runtime runtime;
 Expression expression = Expression(runtime);
 Perception perception = Perception(runtime, expression);
-Motion motion = Motion(runtime, expression);
+Motion motion = Motion(runtime, expression, perception.motion);
 
 void setup() {
 	runtime.init();
@@ -33,14 +34,14 @@ void keep_direction() {
 
 	if (yaw < -10.0f) {
 		if (!moving || movingLeft) {
-			motion.rotateRight();
+			motion.turnRight(motion.NORMAL);
 			movingLeft = false;
 			movingRight = true;
 			expression.say("right");
 		}
 	} else if (yaw > 10.0f) {
 		if (!moving || movingRight) {
-			motion.rotateLeft();
+			motion.turnLeft(motion.NORMAL);
 			expression.say("left");
 			movingLeft = true;
 			movingRight = false;
@@ -53,6 +54,57 @@ void keep_direction() {
 	}
 }
 
+bool going = false;
+
+void avoid_falls() {
+	EnvironmentPerception::Down down = perception.environment.lookDown();
+	if (down == perception.environment.NONE) {
+		motion.freeze();
+		motion.reverse();
+		delay(200);
+		motion.stop();
+		motion.turnLeft(90);
+		going = false;
+	} else if (down == perception.environment.LEFT) {
+		// Only the left-side sensor detects ground.
+		motion.freeze();
+		motion.turnLeft(30);
+		going = false;
+	} else if (down == perception.environment.RIGHT) {
+		// Only the right-side sensor detects ground.
+		motion.freeze();
+		motion.turnRight(30);
+		going = false;
+	} else {
+		if (!going) {
+			motion.go();
+			going = true;
+		}
+	}
+}
+
+void avoid_obstacles() {
+	float distance = perception.environment.lookAhead();
+	if (distance != -1 && distance < 10.0f) {
+		motion.freeze();
+		motion.turnLeft(45);
+		going = false;
+	} else {
+		if (!going) {
+			motion.go();
+			going = true;
+		}
+	}
+}
+
+void echo() {
+	String message = perception.environment.hear();
+	if (message != "") {
+		expression.say(message);
+	}
+}
+
 void loop() {
-	keep_direction();
+	avoid_falls();
+	avoid_obstacles();
 }
